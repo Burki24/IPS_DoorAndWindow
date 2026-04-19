2689ddab1ee5417620e7114835bec59580b944be
Burkhard Kneiseler <burkhard@kneiseler.de>
2026-04-19 08:44:36 +0200

    Implement WindowStateHelper class with state evaluation

--- libs/WindowStateHelper.php
+++ libs/WindowStateHelper.php
@@ -1,1 +1,27 @@
-Test
+<?php
+
+declare(strict_types=1);
+
+class WindowStateHelper
+{
+    public const STATE_CLOSED = 0;
+    public const STATE_TILT   = 1;
+    public const STATE_OPEN   = 2;
+
+    public static function Evaluate(bool $top, bool $bottom): int
+    {
+        if ($top && $bottom) {
+            return self::STATE_OPEN;
+        }
+
+        if ($top && !$bottom) {
+            return self::STATE_TILT;
+        }
+
+        if (!$top && $bottom) {
+            return self::STATE_OPEN; // Sonderfall
+        }
+
+        return self::STATE_CLOSED;
+    }
+}
