085c383e6edca6297f6fcb0ac5509035621dd640
Burkhard Kneiseler <burkhard@kneiseler.de>
2026-04-19 08:44:14 +0200

    Add SensorHelper class with GetState method

--- libs/SensorHelper.php
+++ libs/SensorHelper.php
@@ -1,1 +1,24 @@
-test
+<?php
+
+declare(strict_types=1);
+
+class SensorHelper
+{
+    public static function GetState(int $varID): ?bool
+    {
+        if (!@IPS_VariableExists($varID)) {
+            return null;
+        }
+
+        $value = GetValueBoolean($varID);
+        $var = IPS_GetVariable($varID);
+        $profile = $var['VariableProfile'];
+
+        // Reversed Profile erkennen
+        if (strpos($profile, 'Reversed') !== false) {
+            return !$value;
+        }
+
+        return $value;
+    }
+}
